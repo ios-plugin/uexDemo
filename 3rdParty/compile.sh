00c09f42a19ca6e9303063c95ca38366f1f7ea7c
connyliu <21165491@qq.com>
2016-01-15 16:50:20 +0800

    添加YYWebImage
    默认编译Ono和pop 方便引擎脚本使用

--- 3rdParty/compile.sh
+++ 3rdParty/compile.sh
@@ -27,14 +27,32 @@ function compile_simulator(){
 	fi
 }
 
+function compile_dev(){
+	name=$1
+	if [ -d "./src/$name" ];then
+	rm -rf "tmp"
+	mkdir "tmp"
+	execPath="./output/${name}.framework/${name}"
+	compile_simulator $name
+	mv $execPath "tmp/simu"
+	compile_device $name
+	mv $execPath "tmp/device"
+	lipo -create "tmp/simu" "tmp/device" -output $execPath
+	rm -rf "tmp"
+	fi
+}
+
 for LINE in `cat ./compile_list`  
 do   
 	if [[  $# -gt 0  &&  $var = "simu" ]];then
 	compile_simulator $LINE
+	elif [[  $# -gt 0  &&  $var = "dev" ]];then
+	compile_dev $LINE
 	else
     compile_device $LINE
 	fi
 done
+
 for LINE in `cat ./compile_list`  
 do  
 	if [ -d "./output/${LINE}.framework" ];then
